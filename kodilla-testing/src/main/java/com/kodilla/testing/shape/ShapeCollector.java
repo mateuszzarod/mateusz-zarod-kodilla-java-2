8c25b24dc9cc8da00ce7654fcf741a59ec2d74cb
mateuszzarod <mateusz.zarod@gmail.com>
2018-01-22 19:06:11 +0100

    sixth commit

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -1,28 +1,38 @@
 package com.kodilla.testing.shape;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**Klasa ShapeCollector ma trzy metody
  * jedną dodającą figurę do kolekcji,
  * drugą usuwającą figurę z kolekcji,
  * trzecią pobierającą z kolekcji figurę z pozycji n listy.*/
 
 public class ShapeCollector {
+    ArrayList<Shape> figure = new ArrayList<>();
+
+    //test trzy figury
+    //sprawdził getfigure 1,2,3
+    //sprawdzić czy zwracają dobre wyniki
+
+
     public Shape addFigure(Shape shape){
-    return null;
+        return shape;
     }
 
     public Shape removeFigure(Shape shape){
         return null;
     }
 
-    public int getFigure(int n){
-        return 0;
+    public Shape getFigure(int n){
+        return null;
     }
 
-    public void showFigures(){
-        System.out.println("Figures");
+    public List<Shape> showFigures(){
+        return figure;
     }
 
-
+//czy figura dodana jest ta sama co oczekiwana
 
 
 
