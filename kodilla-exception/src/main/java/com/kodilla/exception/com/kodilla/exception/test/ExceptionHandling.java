dde655af86fea555a1434f39ac2f22a91d892d74
mateuszzarod <mateusz.zarod@gmail.com>
2018-02-19 12:20:48 +0100

    8.3. Throw i Throws - są jakieś różnice?
    Zadanie: Obsługa wyjątku
    Stwórz wewnątrz modułu kodilla-exception pakiet com.kodilla.exception.test, a następnie utwórz klasę SecondChallenge:
    
    Następnie w tym samym pakiecie utwórz klasę ExceptionHandling, w której obsłużysz błąd zgodnie z dobrymi praktykami wykorzystując blok try-catch-finally. Stworzony kod prześlij do repozytorium Github.

--- kodilla-exception/src/main/java/com/kodilla/exception/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,9 +1,14 @@
 package com.kodilla.exception.com.kodilla.exception.test;
 
+/*Następnie w tym samym pakiecie utwórz klasę ExceptionHandling,
+w której obsłużysz błąd zgodnie z dobrymi praktykami wykorzystując
+blok try-catch-finally. Stworzony kod prześlij do repozytorium Github.*/
+
 public class ExceptionHandling {
 
     public static void main(String[] args) {
         SecondChallenge secondChallenge = new SecondChallenge();
+        // w sumie zrobiłem dokładnie to o co chodziło w poleceniu :)
 
         try {
             secondChallenge.probablyIWillThrowException(1.5, 1.5);
--- kodilla-exception/src/main/java/com/kodilla/exception/com/kodilla/exception/test/SecondChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/com/kodilla/exception/test/SecondChallenge.java
@@ -6,6 +6,7 @@ public class SecondChallenge {
     public String probablyIWillThrowException(double x, double y) throws Exception {
         if(x >= 2 || x < 1 || y == 1.5) {
             throw new Exception();
+            //może tutaj być nawet FileReaderExeption (obiekt tej klasy dziedziczy Exception
         }
         return "Done!";
     }
